b513fddd1a7a21e7bd7dcb4316868193ddbb2e4d
Martin Alker <martin.alker@gmail.com>
2015-03-05 19:54:43 +0100

    Optimized Gulp config

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -3,11 +3,9 @@
 var gulp = require('gulp'),
     config = require('../config').jshint,
     jshint = require('gulp-jshint');
-    // plumber = require('gulp-plumber');
 
 gulp.task('jshint', function() {
     gulp.src(config.src)
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'));
-        // .pipe(jshint.reporter('fail'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -14,4 +14,6 @@
 var requireDir = require('require-dir');
 
 // Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
+requireDir('./gulp/tasks', {
+    recurse: true
+});
